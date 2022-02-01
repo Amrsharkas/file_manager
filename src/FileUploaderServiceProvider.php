d9bef4ce90709b130b8a20a387a0b5902ebf9801
islamemam <iemam@qurtobasolutions.com>
2022-02-01 22:52:20 +0200

    fm

--- src/FileUploaderServiceProvider.php
+++ src/FileUploaderServiceProvider.php
@@ -19,12 +19,12 @@ class FileUploaderServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        $this->loadMigrationsFrom(__DIR__.'database/migrations/2022_01_13_134811_create_file_permission_user_table.php');
         $this->loadViewsFrom(__DIR__.'/views','');
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views'),
         ]);
-        $this->mergeConfigFrom(__DIR__.'config/service_configuration.php','service_configuration');
+        $this->mergeConfigFrom(__DIR__.'/config/service_configuration.php','service_configuration');
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views'),
             __DIR__.'/config/service_configuration.php' => config_path('service_configuration'),
