daffdd295ed108c8e9433fc207e52891cf24e654
Islam9595 <iemam@qurtobasolutions.com>
2022-07-01 16:37:48 +0200

    update routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,29 +15,29 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'web'], function () {
-Route::get('/get-dir', 'Ie\FileManager\Http\Controllers\FileManagerController@getDirectory');
-Route::get('/get-tree', 'Ie\FileManager\Http\Controllers\FileManagerController@getTree');
-Route::get('/get-js-tree', 'Ie\FileManager\Http\Controllers\FileManagerController@getDirectoriesForTree');
+Route::get('/get-dir', 'Emam\Filemanager\Http\Controllers\FileManagerController@getDirectory');
+Route::get('/get-tree', 'Emam\Filemanager\Http\Controllers\FileManagerController@getTree');
+Route::get('/get-js-tree', 'Emam\Filemanager\Http\Controllers\FileManagerController@getDirectoriesForTree');
 //ViewController
-Route::get('/preview-text', 'Ie\FileManager\Http\Controllers\ViewController@previewFileAsText');
-Route::get('/preview-media', 'Ie\FileManager\Http\Controllers\ViewController@previewFileAsMedia');
-    Route::get('/preview-media-url', 'Ie\FileManager\Http\Controllers\ViewController@previewFileAsMediaUrl');
+Route::get('/preview-text', 'Emam\Filemanager\Http\Controllers\ViewController@previewFileAsText');
+Route::get('/preview-media', 'Emam\Filemanager\Http\Controllers\ViewController@previewFileAsMedia');
+    Route::get('/preview-media-url', 'Emam\Filemanager\Http\Controllers\ViewController@previewFileAsMediaUrl');
 
-Route::post('/file-upload', 'Ie\FileManager\Http\Controllers\UploadingController@uploadFilesByChunks')->name('files.upload.large');
-Route::get('/rename-file', 'Ie\FileManager\Http\Controllers\UploadingController@renameFileView');
-Route::post('/post-rename-file', 'Ie\FileManager\Http\Controllers\UploadingController@rename');
-Route::get('/remove', 'Ie\FileManager\Http\Controllers\UploadingController@remove');
-Route::get('/move', 'Ie\FileManager\Http\Controllers\UploadingController@moveView');
-Route::get('/copy', 'Ie\FileManager\Http\Controllers\UploadingController@moveView');
-Route::post('/post-move-file', 'Ie\FileManager\Http\Controllers\UploadingController@move');
-Route::post('/post-copy-file', 'Ie\FileManager\Http\Controllers\UploadingController@copy');
+Route::post('/file-upload', 'Emam\Filemanager\Http\Controllers\UploadingController@uploadFilesByChunks')->name('files.upload.large');
+Route::get('/rename-file', 'Emam\Filemanager\Http\Controllers\UploadingController@renameFileView');
+Route::post('/post-rename-file', 'Emam\Filemanager\Http\Controllers\UploadingController@rename');
+Route::get('/remove', 'Emam\Filemanager\Http\Controllers\UploadingController@remove');
+Route::get('/move', 'Emam\Filemanager\Http\Controllers\UploadingController@moveView');
+Route::get('/copy', 'Emam\Filemanager\Http\Controllers\UploadingController@moveView');
+Route::post('/post-move-file', 'Emam\Filemanager\Http\Controllers\UploadingController@move');
+Route::post('/post-copy-file', 'Emam\Filemanager\Http\Controllers\UploadingController@copy');
 
-Route::get('/create-new', 'Ie\FileManager\Http\Controllers\UploadingController@createNewView');
-Route::post('/post-create-file', 'Ie\FileManager\Http\Controllers\UploadingController@createNew');
+Route::get('/create-new', 'Emam\Filemanager\Http\Controllers\UploadingController@createNewView');
+Route::post('/post-create-file', 'Emam\Filemanager\Http\Controllers\UploadingController@createNew');
 
-Route::get('/download-single', 'Ie\FileManager\Http\Controllers\DownloadController@downloadSingle');
-Route::post('/write-file', 'Ie\FileManager\Http\Controllers\UploadingController@writeFile');
+Route::get('/download-single', 'Emam\Filemanager\Http\Controllers\DownloadController@downloadSingle');
+Route::post('/write-file', 'Emam\Filemanager\Http\Controllers\UploadingController@writeFile');
 
-Route::get('/get-compressed-link', 'Ie\FileManager\Http\Controllers\DownloadController@getDownloadCompressedLink');
+Route::get('/get-compressed-link', 'Emam\Filemanager\Http\Controllers\DownloadController@getDownloadCompressedLink');
 
 });
